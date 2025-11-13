70820f009f0a3ec2717b997439b9af0c61d3ca2a
Zeron <dotdot20072607@gmail.com>
2025-11-14 02:24:29 +0500

    Task7

---
+++ Tyuiu.LyapinSE.Sprint4.Task7.V10.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.LyapinSE.Sprint4.Task7.V10.Lib
+{
+    public class DataService : ISprint4Task7V10
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] matrix = new int[n, m];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = int.Parse(value[i * m + j].ToString());
+                }
+            }
+
+            int sum = 0;
+            foreach (int x in matrix)
+            {
+                if (x % 2 == 0)
+                    sum += x;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.LyapinSE.Sprint4.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using Tyuiu.LyapinSE.Sprint4.Task7.V10.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint4.Task7.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new();
+            Assert.AreEqual(30, ds.Calculate(3, 4, "695847142536"));
+        }
+    }
+}
