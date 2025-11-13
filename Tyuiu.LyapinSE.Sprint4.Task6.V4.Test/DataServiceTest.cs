4bf6ff4e1053d7c94166f08e3b4e155308cec614
Zeron <dotdot20072607@gmail.com>
2025-11-14 01:50:06 +0500

    Task6

---
+++ Tyuiu.LyapinSE.Sprint4.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.LyapinSE.Sprint4.Task6.V4.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint4.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new();
+
+            string[] array = ["Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казахстан", "Самара"];
+            string[] expected = ["Москва", "Самара"];
+
+            CollectionAssert.AreEqual(expected, ds.Calculate(array));
+        }
+    }
+}
