2dfd798cffec60a1175250b5fb2fc40fd05a425d
Zeron <dotdot20072607@gmail.com>
2025-11-13 23:14:34 +0500

    Task2

---
+++ Tyuiu.LyapinSE.Sprint4.Task2.V18/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.LyapinSE.Sprint4.Task2.V18.Lib;
+
+Random rnd = new();
+DataService ds = new();
+
+Console.Title = "Спринт #4 | Выполнил: Ляпин С. Е. | ИИПб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #18                                                             *");
+Console.WriteLine("* Выполнил: Ляпин Степан Евгеньевич | ИИПб-25-1                           *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов заполненный         *");
+Console.WriteLine("* случайными в диапазоне от 2 до 8 подсчитать произведение четных         *");
+Console.WriteLine("* элементов массива.                                                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите количество элементов масива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] numsArray = new int[len];
+
+for (int i = 0; i <= len - 1; i++)
+{
+    numsArray[i] = rnd.Next(2, 8);
+}
+
+Console.WriteLine("Массив: ");
+for (int i = 0; i <= len - 1; i++)
+{
+    Console.WriteLine(numsArray[i] + "\t");
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(numsArray);
+
+Console.WriteLine("Произведение чётных элементов = " + res);
+
+Console.ReadKey();
