86fa99ff9d7b0c24e1b201c86dd75f8a71bd708e
Zeron <dotdot20072607@gmail.com>
2025-11-13 21:34:04 +0500

    Add project files.

---
+++ Tyuiu.LyapinSE.Sprint4.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.LyapinSE.Sprint4.Task0.V7.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint4.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestGetMultOddArrEl()
+        {
+            DataService ds = new();
+
+            int[] array = [9, 8, 7, 9, 5, 4, 3, 2, 3, 7];
+
+            Assert.AreEqual(178_605, ds.GetMultOddArrEl(array));
+        }
+    }
+}
