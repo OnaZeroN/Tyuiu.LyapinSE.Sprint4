eea666e2b555e5a50e1cc4c25d672521ed45605d
Zeron <dotdot20072607@gmail.com>
2025-11-14 00:13:23 +0500

    Task3

---
+++ Tyuiu.LyapinSE.Sprint4.Task3.V8.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.LyapinSE.Sprint4.Task3.V8.Lib
+{
+    public class DataService : ISprint4Task3V8
+    {
+        public int Calculate(int[,] array)
+        {
+            int min = array[0, 3];
+            
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (array[i, 3] < min) {
+                    min = array[i, 3];
+                }
+            }
+            return min;
+        }
+    }
+}
