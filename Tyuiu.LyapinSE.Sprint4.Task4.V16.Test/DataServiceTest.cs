4d86982f1f1995cb3d8f8aaee61b50ccc1cf0739
Zeron <dotdot20072607@gmail.com>
2025-11-14 00:54:03 +0500

    Task4

---
+++ Tyuiu.LyapinSE.Sprint4.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.LyapinSE.Sprint4.Task4.V16.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint4.Task4.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new();
+
+            int[,] matrix =
+            {
+                { 6, 5, 6, 7, 3 },
+                { 5, 4, 6, 7, 3 },
+                { 5, 5, 5, 6, 5 },
+                { 6, 5, 4, 4, 3 },
+                { 4, 5, 4, 5, 5 }
+            };
+
+            int[,] waitMatrix =
+            {
+                { 1, 5, 1, 7, 3 },
+                { 5, 1, 1, 7, 3 },
+                { 5, 5, 5, 1, 5 },
+                { 1, 5, 1, 1, 3 },
+                { 1, 5, 1, 5, 5 }
+            };
+
+            CollectionAssert.AreEqual(waitMatrix, ds.Calculate(matrix));
+        }
+    }
+}
